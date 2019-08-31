6cd2ff4f8337979214c912dbd9f273edf99d3caa
kemmitr <russell.kemmit@smail.rasmussen.edu>
2019-08-31 17:45:42 -0500

    first commit

---
+++ DIInstallers/RepositoriesInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using APILegacy.Repository;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace APILegacy.DIInstallers
+{
+    public class RepositoriesInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<ITeacher>().ImplementedBy<TeacherRepository>().LifestyleTransient());
+            container.Register(
+                Component.For<IStudent>().ImplementedBy<StudentRepository>().LifestyleTransient());
+        }
+    }
+}
